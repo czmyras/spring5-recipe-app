2a045a0ed87e23bd7092bcea7e54e38590f77139
Tomasz Czmyr <tomasz.czmyr@bilansjr.pl>
2018-01-21 21:18:20 +0100

    RecipeController - newRecipe

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -1,10 +1,13 @@
 package guru.springframework.controllers;
 
+import guru.springframework.commands.RecipeCommand;
 import guru.springframework.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -25,6 +28,20 @@ public class RecipeController {
         log.debug("Getting recipe: " + id);
         model.addAttribute("recipe", recipeService.findById(new Long(id)));
         return "recipe/show";
+    }
+
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+//    @RequestMapping(name = "recipe", method = RequestMethod.POST)
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand savedRecipe = recipeService.saveRecipeCommand(command);
+        return "redirect:/recipe/show/" + savedRecipe.getId();
     }
 
 }
