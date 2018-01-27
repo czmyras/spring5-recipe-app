85649712b589725a035525364ff01f9934632830
Tomasz Czmyr <tomasz.czmyr@bilansjr.pl>
2018-01-27 12:18:17 +0100

    Rendering image from db

--- src/main/java/guru/springframework/controllers/ImageController.java
+++ src/main/java/guru/springframework/controllers/ImageController.java
@@ -1,7 +1,9 @@
 package guru.springframework.controllers;
 
+import guru.springframework.commands.RecipeCommand;
 import guru.springframework.services.ImageService;
 import guru.springframework.services.RecipeService;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 @Controller
 public class ImageController {
@@ -38,7 +45,32 @@ public class ImageController {
     }
 
 
+    @GetMapping("recipe/{recipeId}/recipeimage")
+    public void renderImageFromDB(@PathVariable String recipeId, HttpServletResponse response) throws IOException {
+
+        RecipeCommand command = recipeService.findCommandById(Long.valueOf(recipeId));
+
+        Byte[] image = command.getImage();
+        byte[] byteArray;
+        if (image != null) {
+            byteArray = new byte[command.getImage().length];
+
+            int i = 0;
+
+            for (Byte wrappedByte : command.getImage()) {
+                byteArray[i++] = wrappedByte;
+            }
+
+            response.setContentType("image/jpeg");
+
+        } else {
+            byteArray = new byte[0];
+
+        }
+        InputStream is = new ByteArrayInputStream(byteArray);
+        IOUtils.copy(is, response.getOutputStream());
 
 
+    }
 
 }
